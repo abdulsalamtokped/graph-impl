741806fbaba1071394b5cfb6f8dbf266e77e46f6
Abdul Salam <110958791+abdulsalamtokped@users.noreply.github.com>
2023-01-24 14:52:28 +0700

    ADD: rebase

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 func main() {
 	graphImplementation := NewGraph()
 
-	graphImplementation.AddVertex(1)
-	graphImplementation.AddVertex(11)
-	graphImplementation.AddVertex(34)
+	graphImplementation.AddVertex(212)
+	graphImplementation.AddVertex(213)
+	graphImplementation.AddVertex(214)
 
-	graphImplementation.AddEdge(1, 11)
-	graphImplementation.AddEdge(11, 34)
-	graphImplementation.AddEdge(34, 44)
+	graphImplementation.AddEdge(212, 213)
+	graphImplementation.AddEdge(212, 214)
+	graphImplementation.AddEdge(213, 214)
+	graphImplementation.AddEdge(214, 213)
 
 	graphImplementation.Print()
 }
